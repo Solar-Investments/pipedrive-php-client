76502c0418180f6e64335f401e0c4173ce7ea1b7
Andrew Poulton <andrew.poulton@solarreviews.com>
2024-02-21 08:40:31 +1030

    Implement changes required to match pipedrive latest API changes.

--- lib/ObjectSerializer.php
+++ lib/ObjectSerializer.php
@@ -372,7 +372,7 @@ class ObjectSerializer
         if (method_exists($class, 'getAllowableEnumValues')) {
             if (!in_array($data, $class::getAllowableEnumValues(), true)) {
                 $imploded = implode("', '", $class::getAllowableEnumValues());
-                throw new InvalidArgumentException("Invalid value for enum '$class', must be one of: '$imploded'");
+                throw new InvalidArgumentException("Invalid value '$data' for enum '$class', must be one of: '$imploded'");
             }
             return $data;
         } else {
